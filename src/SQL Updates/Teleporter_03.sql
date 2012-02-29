8fc58f05151cd1e618d54093b703832d36cf501f
Mathias1000 <mathiasmeinsen@yahoo.de>
2012-02-29 18:06:22 +0100

    add forget line in SQL Updates/Teleporter_03.sql

--- src/SQL Updates/Teleporter_03.sql
+++ src/SQL Updates/Teleporter_03.sql
@@ -1,5 +1,6 @@
 ALTER TABLE teleporter ADD COLUMN `AnswerMap0X` INT( 11 ) NOT NULL ;
 ALTER TABLE teleporter ADD COLUMN `AnswerMap0Y` INT( 11 ) NOT NULL ;
+ALTER TABLE teleporter ADD COLUMN `AnswerMap0` INT( 11 ) NOT NULL ;
 INSERT INTO `teleporter` (`NPCId`, `AnswerMap0`, `AnswerMap1`, `AnswerMap2`, `AnswerMap3`, `AnswerMap0X`, `AnswerMap0Y`, `AnswerMap1X`, `AnswerMap1Y`, `AnswerMap2X`, `AnswerMap2Y`, `AnswerMap3X`, `AnswerMap3Y`) VALUES
 (191, '0', '9', '75', '5', 4150, 4744, 11788, 10395, 9039, 9312, 13660, 7862);
 UPDATE shinenpc SET Flags='2' WHERE MobID='191';
