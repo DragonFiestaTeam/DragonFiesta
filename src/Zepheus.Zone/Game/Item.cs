cb7e8c50384f4f095d811f4c6447093687b7fc27
mathias1000 <mathiasmeinsen@yahoo.de>
2012-04-29 09:22:59 +0200

    add base fr Item Refiment

--- src/Zepheus.Zone/Game/Item.cs
+++ src/Zepheus.Zone/Game/Item.cs
@@ -22,10 +22,18 @@ namespace Zepheus.Zone.Game
         public virtual DateTime? Expires { get; set; }
         // public ItemSlot SlotType { get; private set; }
         public sbyte Slot { get; set; }
-
+        private byte lenght {  get;  set; }
         public ushort Count { get; set; }
         public ItemInfo Info { get { return DataProvider.Instance.GetItemInfo(this.ID); } }
         public Mount Mount { get; set; }
+
+        public byte StatCount { get; private set; }
+        public bool IsEquipped { get; set; }
+        public ushort Str { get; private set; }
+        public ushort End { get; private set; }
+        public ushort Dex { get; private set; }
+        public ushort Int { get; private set; }
+        public ushort Spr { get; private set; }
 
         public Item(uint pOwner, ushort pID, ushort pCount)
         {
@@ -93,9 +101,29 @@ namespace Zepheus.Zone.Game
                 }
             }
         }
+        public byte CalculateDataLen()
+        {
+            byte length = 0;
+            switch (this.Info.Class)
+            {
+                case ItemClass.Rider:
+                    length = 16;
+                    break;
+                case ItemClass.PremiumItem:
+                    break;
+                   case ItemClass.Skillbook:
+                    break;
+                default:
+                    length = 5;
+                    break;
+            }
+            return length;
+        }
         public void WriteItemInfo(Packet pPacket)
         {
-            pPacket.WriteByte(5);//lenght
+            byte lenght = CalculateDataLen();
+            this.lenght = lenght;
+            pPacket.WriteByte(lenght);//lenght
             pPacket.WriteByte((byte)this.Slot);
             pPacket.WriteByte(0x24);//status
             WriteItemStats(pPacket);
@@ -104,9 +132,43 @@ namespace Zepheus.Zone.Game
         public void WriteItemStats(Packet pPacket)
         {
             pPacket.WriteUShort(this.ID);
-            pPacket.WriteByte((byte)this.Count);
+            if (this.lenght == 5)
+            {
+                pPacket.WriteByte((byte)this.Count);
+            }
+            // pPacket.WriteByte((byte)this.Count);
+            WriteItemExtraData(pPacket);
         }
-
+        public void WriteItemExtraData(Packet pPacket)
+        {
+            byte statCount = 0;
+            if (Str > 0) statCount++;
+            if (End > 0) statCount++;
+            if (Dex > 0) statCount++;
+            if (Spr > 0) statCount++;
+            if (Int > 0) statCount++;
+            switch (this.Info.Class)
+            {
+                case ItemClass.Rider:
+                    pPacket.WriteByte(1);   // Pet Refinement Lol
+                    pPacket.Fill(2, 0);                     // UNK
+                    pPacket.WriteUInt(1992027391);               // Expiring time (1992027391 -  never expires)
+                    pPacket.WriteUInt(1992027391);               // Time? (1992027391 -  never expires)
+                    pPacket.WriteByte(1);
+                    break;
+                case ItemClass.PremiumItem:
+                    break;
+                case ItemClass.Skillbook:
+                    break;
+                default:
+                    break;
+            }
+            if (Str > 0) { pPacket.WriteByte(0); pPacket.WriteUShort(Str); }
+            if (End > 0) { pPacket.WriteByte(1); pPacket.WriteUShort(End); }
+            if (Dex > 0) { pPacket.WriteByte(2); pPacket.WriteUShort(Dex); }
+            if (Spr > 0) { pPacket.WriteByte(3); pPacket.WriteUShort(Spr); }
+            if (Int > 0) { pPacket.WriteByte(4); pPacket.WriteUShort(Int); }
+        }
         public static Item LoadItem(DataRow Row)
         {
             ulong id = GetDataTypes.GetUlong(Row["ID"]);
