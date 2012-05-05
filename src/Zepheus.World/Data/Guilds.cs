aeea78edda05838e6809c5868da6ef276e9a6a8d
mathias1000 <mathiasmeinsen@yahoo.de>
2012-05-05 23:47:28 +0200

    fix solution

---
+++ src/Zepheus.World/Data/Guilds.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zepheus.World.Data
+{
+    public class Guild
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<int> GuildMembers = new List<int>();
+        public string GuildPassword { get; set; }
+        public int GuildMaster { get; set; }
+    }
+    public class Guilds
+    {
+        public List<Guild> GuildList { get; set; }
+    }
+}
