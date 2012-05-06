895adae8744f629e94aabb309127f3fb7e15c5a7
mathias1000 <mathiasmeinsen@yahoo.de>
2012-05-06 09:56:29 +0200

    add forgot file

---
+++ src/Zepheus.Zone/Game/Guild/Guild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zepheus.Zone.Game.Guild
+{
+    public sealed class Guild
+    {
+        public GuildNotice GuildNote { get; set; }
+        public int GuildID { get; set; }
+        public DateTime? Date { get; set; }
+        public List<GuildMember> GuildMembers { get; set; }
+    }
+    public sealed class GuildNotice
+    {
+        public string NoticeOwner { get; set; }
+        public string Notice { get; set; }
+    }
+}
---
+++ src/Zepheus.Zone/Game/Guild/GuildMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zepheus.Zone.Game.Guild
+{
+   public sealed class GuildMember
+    {
+       public ZoneCharacter Member { get; set; }
+       public Guild MemberGuild { get; set; }
+    }
+}
