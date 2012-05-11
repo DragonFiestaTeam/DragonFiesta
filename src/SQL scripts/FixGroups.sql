13c556bfafe16308219a2318f780fad9a1051764
unknown <thomson.janpelle@medio-04.mso.local>
2012-05-11 09:28:08 +0100

    Modified the layout of a table.
    FILE: /src/SQL scripts/FixGroups.sql
    	* modified layout

--- src/SQL scripts/FixGroups.sql
+++ src/SQL scripts/FixGroups.sql
@@ -3,7 +3,7 @@
  ****************************************
  * Use this script whenever the groups  *
  * in the database are broken. It will  *
- * repair them automatically.		*
+ * repair them automatically.			*
  ****************************************/
 
 -- mark every group without members as 
@@ -29,12 +29,12 @@ SET
 	`GroupID`	=	NULL,
 	`IsGroupMaster`	=	NULL
 WHERE
-	(SELECT COUNT(*)
-	 FROM `fiesta_world`.`groups`
-	 WHERE
-	 (	`groups`.`Id` = `characters`.`GroupID`	)
-	AND (	`groups`.`Exists` = TRUE )) 
-		<= 0;
+	(
+		SELECT COUNT(*) FROM `fiesta_world`.`groups`
+		WHERE
+		(	`groups`.`Id` = `characters`.`GroupID`)
+		AND (`groups`.`Exists` = TRUE )
+	) <= 0;
 
 -- delete all group columns, that are non existant
 DELETE
